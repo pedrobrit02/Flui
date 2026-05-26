f47707b3537e6ec9f7c3df69421b276ace215011
Pedro Brito <pedro.brito.flui@gmail.com>
2026-05-26 17:01:33 -0300

    Corrigindo grid do CSS, html do carrossel e config do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// Mantenha apenas esta configuração única:
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './',
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        uso: resolve(__dirname, 'uso.html'),
+        emprestimo: resolve(__dirname, 'emprestimo.html')
+      }
+    }
+  }
 })

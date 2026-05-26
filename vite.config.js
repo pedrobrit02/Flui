46137730b1c930a2984ad8c3032f4fe8c4079113
Pedro Brito <pedro.brito.flui@gmail.com>
2026-05-25 22:21:09 -0300

    Atualizando arquivos essenciais e ignorando node_modules

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Mantenha apenas esta configuração única:
 export default defineConfig({
   plugins: [react()],
+  base: './', 
 })
